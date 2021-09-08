a3c8a707d47d3941632535ca669344a2e1d13c20
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-09-08 17:51:01 +0700

    No 3: Sign up page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])  
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      ExampleMailer.sample_email(@user).deliver
+      flash[:info] = "Please check your email to activate your account.\n"
+      redirect_to root_url
+    else
+      render 'new'  
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone_number, :date_of_birth, :gender, :favorite_cinema)
+    end
+end
