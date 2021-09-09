cdf5b8571746a0b6cbb4153d145bfaac18527a39
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-09-10 00:16:39 +0700

    No 4: Login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show]
+
   def show
     @user = User.find(params[:id])  
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
   get 'users/new'
   get 'static_pages/home'
 
+  match '/404', to: 'static_pages#not_found', via: :all
+  match '/500', to: 'static_pages#internal_server_error', via: :all
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  
   root 'static_pages#home'
 end
