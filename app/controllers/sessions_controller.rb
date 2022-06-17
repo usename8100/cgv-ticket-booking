2d4f086dbbb04eb2434261e98ca5b3638f52e7ac
TrinhMinhLong <trinhminhlongad@gmail.com>
2022-06-17 14:15:02 +0700

    fix redirect destination

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+        redirect_back_or movies_path
       else
         message = "Tài khoản chưa được kích hoạt. "
         message += "Vui lòng kiểm tra email để kích hoạt tài khoản của bạn."
